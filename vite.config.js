f554190d180745d10f0298ca5b1d76ca7227f09d
Gumersindo1404 <gumersindo1404@gmail.com>
2024-11-13 13:09:07 +0100

    first commit

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import vituum from "vituum";
+import handlebars from "@vituum/vite-plugin-handlebars";
+
+export default {
+    plugins: [
+        vituum(),
+        handlebars({
+            root: "./src",
+        }),
+    ],
+};
+
